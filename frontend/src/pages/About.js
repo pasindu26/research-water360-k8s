852dea2253ef27c308343f2f19da15188c2cba75
kusal-k8s <root@ip-172-31-27-204.ec2.internal>
2025-02-28 09:08:29 +0000

    check kubectl

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -4,7 +4,7 @@ function Pricing() {
   return (
     <div style={{ padding: '20px' }}>
       <h1>Pricing</h1>
-      <p>Here is our pricing information.</p>
+      <p>Here is our pricing information..</p>
     </div>
   );
 }
